b5c5c2b1a1b02cda79cac183ac1dcfc639807dd3
Jesse Hattabaugh <arkanciscan@gmail.com>
2012-02-01 13:24:21 -0800

    git is weird

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Copyright 2012 Jesse Hattabaugh
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-from google.appengine.ext import webapp
+
+import os, bottle
 from google.appengine.ext.webapp import util
 
+bottle.debug(True)
 
-class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
-
+@bottle.get('/')
+@bottle.view('main')
+def index():
+  return dict()
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+  app = bottle.default_app()
+  util.run_wsgi_app(app)
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+  main()
